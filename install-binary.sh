3fbfc0eb000a591b5700d3acada1f310321f4920
Colin Hoglund <colin.hoglund@mongodb.com>
2018-07-02 15:05:47 -0400

    update executable name

--- install-binary.sh
+++ install-binary.sh
@@ -90,7 +90,7 @@ installFile() {
   HELM_TMP="/tmp/$PROJECT_NAME"
   mkdir -p "$HELM_TMP"
   tar xf "$PLUGIN_TMP_FILE" -C "$HELM_TMP"
-  HELM_TMP_BIN="$HELM_TMP/tpl"
+  HELM_TMP_BIN="${HELM_TMP}/${PROJECT_NAME}"
   echo "Preparing to install into ${HELM_PLUGIN_PATH}"
   # Use * to also copy the file withe the exe suffix on Windows
   cp "$HELM_TMP_BIN"* "$HELM_PLUGIN_PATH"
@@ -113,7 +113,7 @@ testVersion() {
   # To avoid to keep track of the Windows suffix,
   # call the plugin assuming it is in the PATH
   PATH=$PATH:$HELM_PLUGIN_PATH
-  tpl -h
+  ${PROJECT_NAME} -h
   set -e
 }
 
